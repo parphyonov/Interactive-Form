cba22a9b3d9d747e0a7bfabb97cf4667351c6616
Alexei Parphyonov <alexei.s.parphyonov@gmail.com>
2018-10-05 17:37:59 +0300

    include a text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu. It wil also be shown when JS is disabled

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 // Sets focus on the first input element
 $('#name').attr('autofocus', true);
+
+// Hides #other-title from the view, so that it appears only when JS is disabled
+$('#other-title').toggle();
